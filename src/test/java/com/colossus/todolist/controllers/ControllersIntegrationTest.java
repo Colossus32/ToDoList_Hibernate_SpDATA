1f99ffd61e71a9e1793ed4b607dcee07ff00b7af
Kirill Kosykh <triplesonicslash@list.ru>
2022-06-04 18:55:18 +0300

    Integration testing correcting

--- src/test/java/com/colossus/todolist/controllers/ControllersIntegrationTest.java
+++ src/test/java/com/colossus/todolist/controllers/ControllersIntegrationTest.java
@@ -15,9 +15,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -73,18 +72,41 @@ public class ControllersIntegrationTest {
     @Test
     public void getUser() throws Exception{
 
+        this.mockMvc.perform(get("/user/{id}", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("email").value("updatedEmail"))
+                .andExpect(jsonPath("password").value("456"))
+                .andExpect(jsonPath("id").value(1));
+    }
+
+    @Test
+    public void updateUser() throws Exception{
+
         this.mockMvc.perform(post("/user/registration")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
-                .content("{\"email\": \"email\", \"password\": \"123\"}"))
-                .andDo(print());
+                .content("{\"email\": \"email@gmail.com\", \"password\": \"123\"}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("email").value("email@gmail.com"))
+                .andExpect(jsonPath("password").value("123"))
+                .andExpect(jsonPath("id").isNotEmpty());
+
+
+        this.mockMvc.perform(put("/user/{id}", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content("{\"email\": \"updatedEmail\", \"password\": \"456\"}"))
+                .andDo(print())
+                .andExpect(status().isOk());
 
         this.mockMvc.perform(get("/user/{id}", "1"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("email").value("email"))
-                .andExpect(jsonPath("password").value("123"))
+                .andExpect(jsonPath("email").value("updatedEmail"))
+                .andExpect(jsonPath("password").value("456"))
                 .andExpect(jsonPath("id").value(1));
     }
-
 }
